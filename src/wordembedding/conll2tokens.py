38417a7344983ec9f7c65ba3333077c9ba6b836f
lca80 <david@emtelligent.com>
2018-07-31 17:38:25 -0700

    counter

--- src/wordembedding/conll2tokens.py
+++ src/wordembedding/conll2tokens.py
@@ -7,24 +7,33 @@ default_in_dir = "../../cas-reader/output_dir -o tokenized_sentences"
 default_in_dir = "/shared/dropbox/ctakes_conll/output/clinical/dc_summaries"
 default_out_dir = "tokenized_sentences"
 
+def init(args):
+	global counter
+	counter = args
+
 def func(x):
-    (i, in_dir, out_dir, f) = x
-    with open(os.path.join(in_dir,f), 'r') as fd:
-        lines = fd.read().split('\n')
+	global counter
 
-    tokens = [line.split(' ')[0] for line in lines]
-    doc_str = " ".join(tokens).replace("  ", "\n").strip()
+	(i, in_dir, out_dir, f) = x
+	with open(os.path.join(in_dir,f), 'r') as fd:
+		lines = fd.read().split('\n')
 
-    if doc_str == "":
-        return None
+	tokens = [line.split(' ')[0] for line in lines]
+	doc_str = " ".join(tokens).replace("  ", "\n").strip()
+
+	if doc_str == "":
+		return None
 	
-    out_dir = os.path.join(out_dir,f)
-    with open(out_dir, 'w') as fd:
-        fd.write(doc_str)
+	out_dir = os.path.join(out_dir,f)
+	with open(out_dir, 'w') as fd:
+		fd.write(doc_str)
 
-    if i%10 == 0:
-        print ("\rfiles:%2d"%(i), end="    ")
-        sys.stdout.flush()
+	if counter.value % 10 == 0:
+		print ("\rfiles:%2d"%(i), end="	")
+		sys.stdout.flush()
+
+	with counter.get_lock():
+		counter.value += 1
 
 	return  out_dir
 
@@ -50,6 +59,7 @@ if __name__ == '__main__':
 
 	idx_fs = [ (i, in_dir, out_dir, f) for i, f in enumerate(os.listdir(in_dir)[:args.limit])]
 	#ret_lst = [func(x) for x in idx_fs]
-	ret_lst = multiprocess.Pool().imap_unordered(func, idx_fs)
+	counter = multiprocessing.Value('i', 0)
+	ret_lst = multiprocessing.Pool(initializer=init, initargs=(counter,) ).imap_unordered(func, idx_fs)
 	ret_lst = [r for r in ret_lst if r != None]
 	print ("\ntotal files:", len(ret_lst), "\nsave to:", out_dir)
